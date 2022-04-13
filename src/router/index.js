40a0281d0024a66a70302bb4f3447390ed955a67
zheng <liao_brant@163.com>
2022-04-13 22:01:36 +0800

    perf: 优化路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,63 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-const Home = () => import('../views/Home/Home')
-const Detail = () => import('../views/Detail/Detail')
-const WorldMap = () => import("../views/WorldMap/WorldMap")
-const ChinaMap = () => import("../views/ChinaMap/ChinaMap")
-const Bank = () => import('../views/Bank/Bank')
-const Room = () => import('../views/Room/Room')
-const ProvinceCharts = () => import('../views/ProvinceCharts/ProvinceCharts')
-const Start = () => import('../views/Start/Start')
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+const Home = () => import(/* webpackChunkName: "home" */ "../views/Home/Home");
+const Detail = () =>
+  import(/* webpackChunkName: "detail"*/ "../views/Detail/Detail");
+const WorldMap = () =>
+  import(/* webpackChunkName: "world" */ "../views/WorldMap/WorldMap");
+const ChinaMap = () =>
+  import(/* webpackChunkName: "ChinaMap" */ "../views/ChinaMap/ChinaMap");
+const Bank = () => import(/* webpackChunkName: "Bank" */ "../views/Bank/Bank");
+const Room = () => import(/* webpackChunkName: "Room" */ "../views/Room/Room");
+const ProvinceCharts = () =>
+  import(
+    /* webpackChunkName: "ProvinceCharts" */ "../views/ProvinceCharts/ProvinceCharts"
+  );
+const Start = () =>
+  import(/* webpackChunkName: "Start" */ "../views/Start/Start");
+  
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: '/Start'
+    path: "/",
+    redirect: "/Start",
   },
   {
-    path: '/Start',
-    name: 'Start',
-    component: Start
+    path: "/Start",
+    name: "Start",
+    component: Start,
   },
   {
-    path: '/Home',
-    name: 'Home',
+    path: "/Home",
+    name: "Home",
     component: Home,
-    redirect: '/WorldMap',
+    redirect: "/WorldMap",
     children: [
-      { path: '/WorldMap', component: WorldMap },
-      { path: '/ChinaMap', component: ChinaMap },
-      { path: '/ProvinceCharts', component: ProvinceCharts }
-    ]
+      {path: "/WorldMap", component: WorldMap},
+      {path: "/ChinaMap", component: ChinaMap},
+      {path: "/ProvinceCharts", component: ProvinceCharts},
+    ],
   },
   {
-    path: '/Bank',
-    name: 'Bank',
-    component: Bank
+    path: "/Bank",
+    name: "Bank",
+    component: Bank,
   },
   {
-    path: '/Room',
-    name: 'Room',
-    component: Room
+    path: "/Room",
+    name: "Room",
+    component: Room,
   },
   {
-    path: '/Detail',
-    name: 'Detail',
-    component: Detail
+    path: "/Detail",
+    name: "Detail",
+    component: Detail,
   },
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
